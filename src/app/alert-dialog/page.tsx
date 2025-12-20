eb1f16ac1b2e0781b9f53b4e3c328d1aed06f290
koshishthapa517-crypto <koshishthapa517@gmail.com>
2025-12-20 16:56:57 +0545

    Shadcn/UI

---
+++ src/app/alert-dialog/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+const page = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <AlertDialog>
+        <AlertDialogTrigger>
+          <button className="px-4 py-2 bg-black text-white rounded ">Open</button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  )
+}
+
+export default page
